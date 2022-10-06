0ae27e6bdb2c785d10693eb00c6d57ffccd53af5
odd <80826558+kevin-krause@users.noreply.github.com>
2022-10-06 20:29:39 -0300

    feat: initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify
+import json
+import pandas as pd
+
+app = Flask(__name__)
+
+# Cosntruir as funcionalidades
+@app.route('/')
+def pegardados():
+  dadosAPI = pd.read_csv('2022\py\Simple-TF\Heatmap\dados\dados.csv')
+  dadosAPI = (dadosAPI.to_string())
+  
+
+  resposta= {'dados': dadosAPI}
+
+  return jsonify(resposta)
+
+ 
+
+# Rodar API
+app.run()
+
+# tabela = pd.read_csv('2022\py\Simple-TF\Heatmap\dados\dados.csv')
+
