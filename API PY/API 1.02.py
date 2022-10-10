b46cd57608ac54f0a572b57e539f7f1b6844d50e
odd <80826558+kevin-krause@users.noreply.github.com>
2022-10-10 16:18:58 -0300

    feat: API 1.02

---
+++ API PY/API 1.02.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+
+# Cosntruir as funcionalidades
+
+
+@app.route('/sinais')
+def pegardados_sinal():
+    dadosAPI = pd.read_json(
+        'C:/Codding Hub/2022/py/Simple-TF/Heatmap/sos/dados/dados.json')
+    #dadosAPI = (dadosAPI.to_string())
+    dadosAPI = dadosAPI.values.tolist()
+    '''print (dadosAPI)'''
+
+    for dado in dadosAPI:
+        # cada indice representa uma ação da lista - quando for trocar a ação, troque o indice
+        resposta = dadosAPI[1]
+
+        return resposta
+
+
+@app.route('/acao')
+def pegardados_ticker():
+    dadosAPI = pd.read_json(
+        'C:/Codding Hub/2022/py/Simple-TF/Heatmap/sos/dados/dados.json')
+    #dadosAPI = (dadosAPI.to_string())
+    dadosAPI = pd.DataFrame(dadosAPI)
+    dadosColuna = dadosAPI.iloc[-1]
+    '''print (dadosColuna.first_valid_index())'''
+    return dadosColuna.last_valid_index()  # cada indice representa uma ação da lista - quando for trocar a ação, troque o indice
+
+
+# Rodar API
+app.run()
+
+# tabela = pd.read_csv('2022/py/Simple-TF/Heatmap/dados/dados.csv')
--- consumindo api test/consumo.py
+++ consumindo api test/consumo.py
@@ -3,8 +3,9 @@ resposta = {}
 resposta = requests.get('http://127.0.0.1:5000/')
 
 
-json_data = json.loads(resposta.text)
+json_data = json.loads(resposta)
 
 
 
-print(json.dumps(json_data, indent=4)[22:26])
+print(json.dumps(json_data, indent=4)[0])
+
